ee6ac91b8cf6707cd3adfc05130d967b6d39ffcb
michael-n-cooper <cooper@w3.org>
2018-06-26 00:51:14 +0000

    Generated by TRAVIS-CI

---
+++ prototypes/ConformancePrototype/respec-config.js
@@ -0,0 +1,90 @@
+var respecConfig = {
+	// embed RDFa data in the output
+	trace: true,
+	doRDFa: '1.1',
+	includePermalinks: true,
+	permalinkEdge: true,
+	permalinkHide: false,
+	// specification status (e.g., WD, LC, NOTE, etc.). If in doubt use ED.
+	specStatus: "ED",
+	noRecTrack: true,
+	//crEnd:                "2012-04-30",
+	//perEnd:               "2013-07-23",
+	//publishDate:          "2013-08-22",
+	diffTool: "http://www.aptest.com/standards/htmldiff/htmldiff.pl",
+	
+	// the specifications short name, as in https://www.w3.org/TR/short-name/
+	shortName: "silver-requirements",
+	
+	
+	// if you wish the publication date to be other than today, set this
+	// publishDate:  "2009-08-06",
+	copyrightStart: "2018",
+	license: "document",
+	
+	// if there a publicly available Editors Draft, this is the link
+	edDraftURI: "https://w3c.github.io/silver/requirements/",
+	
+	// if this is a LCWD, uncomment and set the end of its review period
+	// lcEnd: "2012-02-21",
+	
+	// editors, add as many as you like
+	// only "name" is required
+	editors:[ {
+		name: "Jeanne Spellman",
+		mailto: "jspellman@spellmanconsulting.com",
+		company: "The Paciello Group, LLC",
+		companyURI: "https://www.paciellogroup.com/",
+		w3cid: 42417
+	}, {
+		name: "Shawn Lauriat",
+		mailto: "lauriat@google.com",
+		company: "Google, Inc.",
+		companyURI: "https://www.google.com/",
+		w3cid: 90646
+	}, {
+				name: "Michael Cooper",
+		mailto: "cooper@w3.org",
+		company: "W3C",
+		companyURI: "https://www.w3.org/",
+		w3cid: 34017
+	}],
+	
+	// authors, add as many as you like.
+	// This is optional, uncomment if you have authors as well as editors.
+	// only "name" is required. Same format as editors.
+	
+	//authors:  [
+	//    { name: "Your Name", url: "http://example.org/",
+	//      company: "Your Company", companyURI: "http://example.com/" },
+	//],
+	
+	/*
+	alternateFormats: [
+	{ uri: 'aria-diff.html', label: "Diff from Previous Recommendation" } ,
+	{ uri: 'aria.ps', label: "PostScript version" },
+	{ uri: 'aria.pdf', label: "PDF version" }
+	],
+	 */
+	
+	// errata: 'https://www.w3.org/2010/02/rdfa/errata.html',
+	
+	// name of the WG
+	wg: "Web Content Accessibility Guidelines Working Group",
+	
+	// URI of the public WG page
+	wgURI: "https://www.w3.org/WAI/GL/",
+	
+	// name (with the @w3c.org) of the public mailing to which comments are due
+	wgPublicList: "public-silver",
+	
+	// URI of the patent status for this WG, for Rec-track documents
+	// !!!! IMPORTANT !!!!
+	// This is important for Rec-track documents, do not copy a patent URI from a random
+	// document unless you know what you're doing. If in doubt ask your friendly neighbourhood
+	// Team Contact.
+	wgPatentURI: "https://www.w3.org/2004/01/pp-impl/35422/status",
+	maxTocLevel: 4,
+	
+	//localBiblio: biblio,
+};
---
+++ prototypes/readme.md
@@ -0,0 +1,25 @@
+
+# Silver Community Group Prototypes
+
+The Silver Task Force and Silver Community Group are soliciting and building prototypes of the future Silver design and structure.  
+
+You are welcome to contribute to ideas and comments to the work. See the instructions below to submit a prototype.
+
+## List of Prototypes To Date
+
+This list is updated sporadicly and could be behind. Use the Code tab in Github to explore the /prototypes/ folder.
+
+Prototype Title | Developed By | Notes | Status
+------------ | ------------- | ----- | ------
+[ConformancePrototype](/prototypes/ConformancePrototype) | @jspellman |  Developed from Silver Design Sprint, Silver member input, and AccessU meeting public input |  In Progress
+
+## Instructions for Prototypes
+Thank you for helping us with developing or refining prototypes for Silver, the successor to WCAG. Before creating any prototypes for Silver:
+
+* Please review the [Suggestions from the Design Sprint](https://www.w3.org/community/silver/2018/04/23/suggestions-of-silver-design-sprint/) for an overall idea of the directions Silver is planning to take.
+* Review the [Design Sprint Report](https://www.w3.org/community/silver/draft-final-report-of-silver/)
+
+If this is a new prototype, create a new branch with the name of: **prototype-[short-name-of prototype]**. Each prototype should:
+* Be contained in its own folder present in the main **protoypes** folder
+* Be accessed initially through an **index.html** page
+* Have an accompanying **readme.md** file containing any relevant information (please use the template provided)
